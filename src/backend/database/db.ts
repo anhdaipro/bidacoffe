e84392676490d78d776a761c17039ac9cc71fdf8
Pham Dai <daip6690@gmail.com>
2025-06-20 14:53:07 +0700

    first commit

--- src/backend/database/db.ts
+++ src/backend/database/db.ts
@@ -1,14 +1,16 @@
 import { Sequelize } from 'sequelize';
-
+const password = process.env.AIVEN_PASSWORD;// Kết nối với MySQL
+const host = process.env.HOST
 // Kết nối với MySQL
-const sequelize = new Sequelize('bida', 'root', 'anhdai123', {
-  host: 'localhost', // Địa chỉ MySQL server
+const sequelize = new Sequelize('bida', 'avnadmin', password, {
+  host: host, // Địa chỉ MySQL server
   dialect: 'mysql',  // Sử dụng MySQL
   logging: false,    // Tắt log SQL (tuỳ chọn)
+  port: 18623,
   timezone: '+07:00', // múi giờ VN
   dialectOptions: {
     dateStrings: true,
-    typeCast: true
+    typeCast: true,
   },
   dialectModule: require('mysql2'),
 });
