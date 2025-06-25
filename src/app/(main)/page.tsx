082df00773ed5a3c53d14013d9dddba11679a342
Pham Dai <daip6690@gmail.com>
2025-06-25 08:54:17 +0700

    daipv

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,12 +7,16 @@ import { useAuthStore } from "../store/useUserStore";
 import Dashboard from "./dashboard/page";
 export default function Home() {
   const user = useAuthStore(state => state.user);
+  
   const router = useRouter();
+  const   isHydrated = useAuthStore(state=>state.isHydrated)
+  console.log(isHydrated)
   useEffect(()=>{
+    if(isHydrated){return}
     if(!user){
       router.push('/login')
     }
-  }, [user])
+  }, [user,isHydrated])
   return (
     <Dashboard/>
   );
--- src/app/store/useUserStore.ts
+++ src/app/store/useUserStore.ts
@@ -1,21 +1,24 @@
 import { create } from 'zustand';
 
-import { persist } from 'zustand/middleware'
+import { createJSONStorage, persist } from 'zustand/middleware'
 export interface User{
   name:string;
   id:number;
-  roleId:number
+  roleId:number;
 }
 interface Props {
   user: User | null;
   setUser: (user: any) => void;
   logout: () => void;
+  isHydrated:boolean;
+
 }
 
 export const useAuthStore = create<Props>()(
   persist(
     (set) => ({
       user: null,
+      isHydrated:false,
       setUser: (user) => set({ user }),
       logout: () => {
         localStorage.removeItem('token');
@@ -25,6 +28,11 @@ export const useAuthStore = create<Props>()(
     }),
     {
       name: 'auth-storage', // 👈 tên key trong localStorage
+            storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+
+      onRehydrateStorage: (state) => {
+        return () => state.isHydrated = true
+      }
     }
   )
 );
